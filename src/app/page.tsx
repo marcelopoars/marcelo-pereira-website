166d3ccd982ac5465bb1c2def4dca513ad6253ed
Marcelo Pereira <x654766@gruposantander.com>
2023-02-07 09:02:12 -0300

    refactor: edited responsive styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import ImageProfile from "../../public/marcelo-pereira-frontend-developer.jpg";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center text-center gap-4 pt-10">
-      <div className="h-[160px] w-[160px] bg-indigo-900 border-8 border-indigo-900 rounded-full overflow-hidden">
+    <section className="flex flex-col items-center text-center gap-4">
+      <div className="w-[120px] 2xl:w-[160px] bg-indigo-900 border-8 border-indigo-900 rounded-full overflow-hidden">
         <Image
           src={ImageProfile}
           alt="Foto Marcelo Pereira"
@@ -20,7 +20,7 @@ export default function Home() {
         <span>Desenvolvedor Frontend</span>
       </h1>
 
-      <div className="max-w-2xl text-xl">
+      <div className="max-w-2xl text-md 2xl:text-xl">
         <p>
           Desenvolvedor Frontend a mais de 14 anos, atualmente trabalhando com
           ReactJs, TypeScript, JavaScript, Styled-components, entre outras
--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -9,7 +9,7 @@ export const SocialLinks = () => {
   ];
 
   return (
-    <aside className="flex justify-center flex-wrap gap-4 pt-8 pb-20">
+    <aside className="flex justify-center flex-wrap gap-4 pt-8">
       {links.map(({ name, url }) => (
         <Link
           key={url}
