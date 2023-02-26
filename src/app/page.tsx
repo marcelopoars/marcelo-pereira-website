248d89e9988c45853b742d280c91f68a0d75d34d
Marcelo Pereira <x654766@gruposantander.com>
2023-02-26 18:02:30 -0300

    chore: edited styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,30 @@ import ImageProfile from "../../public/marcelo-pereira-frontend-developer.jpg";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center text-center gap-4 py-8">
-      <div className="w-[120px] bg-indigo-900 border-4 border-indigo-900 rounded-full overflow-hidden">
-        <Image
-          src={ImageProfile}
-          alt="Foto Marcelo Pereira"
-          placeholder="blur"
-          priority
-        />
-      </div>
+    <section className="flex flex-col items-center text-center gap-12 py-8">
+      <article className="flex flex-col items-center gap-4">
+        <div className="w-[120px] 2xl:w-[160px] bg-indigo-900 border-4 border-indigo-900 rounded-full overflow-hidden relative">
+          <Image
+            src={ImageProfile}
+            alt="Foto Marcelo Pereira"
+            placeholder="blur"
+            width={1000}
+            height={1000}
+            priority
+          />
+        </div>
 
-      <h1>
-        Marcelo Pereira
-        <span>Desenvolvedor Frontend</span>
-      </h1>
+        <h1>
+          Marcelo Pereira
+          <span>Desenvolvedor Frontend</span>
+        </h1>
 
-      <p className="text-base lg:text-lg">
-        Desenvolvedor Frontend a mais de 10 anos, atualmente trabalhando com
-        ReactJs, TypeScript, JavaScript, NextJs, TailwindCss, Styled-components,
-        entre outras tecnologias.
-      </p>
+        <p className="text-base lg:text-lg">
+          Desenvolvedor Frontend a mais de 10 anos, atualmente trabalhando com
+          ReactJs, TypeScript, JavaScript, NextJs, TailwindCss,
+          Styled-components, entre outras tecnologias.
+        </p>
+      </article>
 
       <SocialLinks />
     </section>
--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export const SocialLinks = () => {
   const links = [
     { name: "GitHub", url: "https://github.com/marcelopoars" },
-    { name: "Linkedin", url: "https://linkedin.com/in/marcelopoars/" },
+    { name: "Linkedin", url: "https://linkedin.com/in/marcelopoars" },
     { name: "Youtube", url: "https://youtube.com/DicaDoNerd" },
-    { name: "Instagram", url: "https://instagram.com/dicadonerd/" },
+    { name: "Instagram", url: "https://instagram.com/dicadonerd" },
   ];
 
   return (
-    <aside className="flex justify-center flex-wrap gap-4 pt-8">
+    <aside className="flex justify-center flex-wrap gap-4">
       {links.map(({ name, url }) => (
         <Link
           key={url}
