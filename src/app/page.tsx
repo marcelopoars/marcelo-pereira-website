f93736445d3708d426e4422db380cdcaa792676b
Marcelo Pereira <x654766@gruposantander.com>
2023-02-26 09:46:07 -0300

    chore: optimized images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,21 @@ import { SocialLinks } from "@/components";
 
 import blurImage from "../../public/blur.png";
 
-
-const ImageProfileBase64 = blurImage.blurDataURL
+const ImageProfileBase64 = blurImage.blurDataURL;
 
 console.log(ImageProfileBase64);
 
-
 export default function Home() {
   return (
-    <section className="flex flex-col items-center text-center gap-4">
-      <div className="w-[120px] 2xl:w-[160px] bg-indigo-900 border-4 border-indigo-900 rounded-full overflow-hidden">
+    <section className="flex flex-col items-center text-center gap-4 py-8">
+      <div className="w-[120px] bg-indigo-900 border-4 border-indigo-900 rounded-full overflow-hidden">
         <Image
           src={"/marcelo-pereira-frontend-developer.jpg"}
           alt="Foto Marcelo Pereira"
-          width={160}
-          height={160}
+          width={120}
+          height={120}
           placeholder="blur"
           blurDataURL={ImageProfileBase64}
-          quality={100}
-          priority
           sizes="(min-width: 60em) 24vw, (min-width: 28em) 45vw, 100vw"
         />
       </div>
@@ -35,7 +31,7 @@ export default function Home() {
       </h1>
 
       <div className="max-w-2xl">
-        <p className="text-lg">
+        <p className="text-base lg:text-lg">
           Desenvolvedor Frontend a mais de 14 anos, atualmente trabalhando com
           ReactJs, TypeScript, JavaScript, Styled-components, entre outras
           tecnologias.
