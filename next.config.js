8c0e4310454fa0acdf3dd49e3e49076e911375ec
Marcelo Pereira <x654766@gruposantander.com>
2023-02-26 09:50:09 -0300

    chore: optimized images

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [40, 40, 40, 40, 40, 80, 160, 320],
+    formats: ['image/avif', 'image/webp'],
   },
 }
 
