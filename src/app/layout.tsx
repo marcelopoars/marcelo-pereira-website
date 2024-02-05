86e15efd30b3148c3e93b6305649d21e0851b4af
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-02-05 14:13:05 -0300

    refactor: add min-h-dvh style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   title: "Marcelo Pereira | Desenvolvedor Frontend ReactJs",
   description:
     "Desenvolvedor Frontend a mais de 10 anos, atualmente trabalhando com ReactJs, TypeScript, JavaScript, NextJs, TailwindCss, Styled-components, entre outras tecnologias.",
-    metadataBase: new URL('https://marcelopereira.dev'),
+  metadataBase: new URL("https://marcelopereira.dev"),
 };
 
 export default function RootLayout({
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br" className="antialiased">
-      <body className={`${inter.className} min-h-svh`}>
+      <body className={inter.className}>
         <main className="max-w-[800px] mx-auto flex justify-center items-center flex-1 px-6">
           {children}
         </main>
