73852bbac45285329754b0e484714c498071daa7
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-02-19 14:18:01 -0300

    chore: update projects

--- src/app/lib/my-rojects.ts
+++ src/app/lib/my-rojects.ts
@@ -6,7 +6,7 @@ import notesNlwExpertThumb from '@/app/assets/projects/notes-nlw-expert-app.png'
 export const myProjects = [
   {
     name: 'Tailwind NextJs',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ducimus exercitationem ipsa cumque iusto cum in ffooo bar foo foo?',
+    description: 'Projeto de estudo desenvolvido com NextJs e TailwindCSS.',
     githubLink: 'https://github.com/marcelopoars/tailwind-nextjs',
     previewLink: 'https://tailwind-nextjs.marcelopereira.dev',
     stack: ['ReactJs', 'NextJs', 'TypeScript', 'TailwindCSS'],
@@ -15,7 +15,7 @@ export const myProjects = [
   },
   {
     name: 'Notes',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet ducimus exercitationem ipsa cumque iusto cum in?',
+    description: 'Aplicação front-end em ReactJS que permite o usuário salvar notas/lmbretes. As notas podem ser criadas tanto escritas como gravadas por voz.',
     githubLink: 'https://github.com/marcelopoars/nlw-experts-notes',
     previewLink: 'https://notes.marcelopereira.dev',
     stack: ['ReactJs', 'NextJs', 'TypeScript', 'TailwindCSS'],
@@ -24,19 +24,19 @@ export const myProjects = [
   },
   {
     name: 'Elaine | Costureira',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    githubLink: 'https://github.com/marcelopoars/elaine-costureira-website',
+    description: 'Landing page para divulgar os serviços de costura da Elaine.',
+    githubLink: '',
     previewLink: 'https://www.elainecostureira.com.br',
-    stack: ['ReactJs', 'NextJs', 'TypeScript', 'TailwindCSS'],
+    stack: ['ReactJs', 'NextJs', 'TypeScript', 'TailwindCSS', 'SEO'],
     thumb: elaineCostureiraThumb,
     projectType: 'Website'
   },
   {
     name: 'Triplo A Seminovos',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Website desenvolvido para o cliente Triplo A Seminovos, revendedora de veículos de Canoas-RS.',
     githubLink: '',
     previewLink: 'https://triploaseminovos.com.br',
-    stack: ['ReactJs', 'GatsbyJs', 'JavaScript', 'Styled-components'],
+    stack: ['ReactJs', 'GatsbyJs', 'JavaScript', 'Styled-components', 'SEO'],
     thumb: triploASeminovosThumb,
     projectType: 'Website'
   }
