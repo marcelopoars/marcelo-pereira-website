97a3d64550f562a76f8f1bb7754ea5b2f11f0298
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2024-03-09 20:21:57 -0300

    chore: add metadata configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import { Footer, Header } from "./components";
 
 import "./styles/globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -12,10 +13,24 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
+  generator: "Next.js",
+  applicationName: "Marcelo Pereira - Website",
+  category: 'technology',
+  authors: [{ name: "Marcelo Pereira", url: "https://marcelopereira.dev" }],
+  creator: "Marcelo Pereira",
   title: "Marcelo Pereira | Desenvolvedor Frontend",
   description:
-    "Criação e desenvolvimento de sites, landing pages e aplicações web.",
+    "Criação e desenvolvimento de sites, landing pages e aplicações web em Porto Alegre.",
   metadataBase: new URL("https://marcelopereira.dev"),
+  alternates: {
+    canonical: "/",
+    languages: { pt: "/" },
+  },
+  openGraph: {
+    type: "website",
+    countryName: "Brasil",
+    url: "https://marcelopereira.dev",
+  },
 };
 
 export default function RootLayout({
@@ -30,6 +45,7 @@ export default function RootLayout({
         <main className="flex-1">{children}</main>
         <Footer />
         <Analytics />
+        <GoogleTagManager gtmId="G-VBFCXZR5JB" />
       </body>
     </html>
   );
